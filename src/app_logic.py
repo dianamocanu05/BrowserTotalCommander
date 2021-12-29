3a757a4e8c7b269a925727c274b8ed56d1eb0ddf
dianamocanu05 <diana.paula.mocanu@gmail.com>
2021-12-29 18:02:58 +0200

    experimenting with flask

--- src/app_logic.py
+++ src/app_logic.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render
-from django import template
 from src.file_manager import get_file_tree
 from src.utils import create_html_list
+from flask import Flask, render_template
 
-linker = template.Library()
+app = Flask(__name__, template_folder='templates')
 
 
-@linker.assignment_tag
-def hello(request):
+@app.route('/')
+def hello():
     tree = get_file_tree("../root")
     html_code = create_html_list(tree, tree[0])
-    return render(request, 'template.html', {'list': html_code})
+    return render_template('template.html', subprocess_output={'list': html_code})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
